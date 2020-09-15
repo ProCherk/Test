4331d6dd0c82f60a393fb4d29e6719722b7650f4
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-09-15 12:19:48 +0300

    New update

--- Test/Program.cs
+++ Test/Program.cs
@@ -5,8 +5,12 @@ namespace Test
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Console.WriteLine("Введите текст: ");
+            string a = Console.ReadLine();
             Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
+
         }
     }
 }
